c0ca16e875c5a914f6888958adee2c663bc6f2e7
CYBER WARRIOR <134825125+CYBER-WARRIIOR@users.noreply.github.com>
2025-01-06 12:05:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 
 //💌global.pairingNumber = "" //put your bot number here💌
-global.mods = ['923042205427'] 
+global.mods = [923140053156'] 
 global.prems = ['']
 global.allowed = ['']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
@@ -118,22 +118,22 @@ global.princeImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen
 
 
 // Moderator 
-global.developer = 'https://wa.me/message/DCAK67ON3XVOG1' //contact
+global.developer = 'https://wa.me/message/923140053156' //contact
 //💌------------------------------------------💌
 
 
 
 //Sticker WM
 global.botname = process.env.BOT_NAME
-global.princebot = '🛡️𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋🛡️'
+global.princebot = '✦ 𝐂𝐲𝐛𝐞𝐫 𝐂𝐫𝐲𝐩𝐭𝐨 𝐊𝐢𝐧𝐠°🪙°👑'
 global.packname = process.env.PACK_NAME
-global.author = 'Prince♥️' 
+global.author = '✦ 𝐂𝐲𝐛𝐞𝐫 𝐂𝐫𝐲𝐩𝐭𝐨 𝐊𝐢𝐧𝐠°🪙°👑' 
 global.princeig = 'https://www.instagram.com' 
-global.princegp = 'https://whatsapp.com/channel/0029VaKNbWkKbYMLb61S1v11'
-global.menuvid = 'https://i.imgur.com/GFAAXqw.mp4'
-global.Princesc = 'https://github.com/PRINCE-GDS/THE-PRINCE-BOT' 
+global.princegp = 'https://whatsapp.com/channel/0029VagNVUxDOQIXdDYKR61J'
+global.menuvid = 'https://i.imgur.com/89m3j04.mp4'
+global.Princesc = 'https://github.com/CYBER-WARRIIOR/CYBER-P.B' 
 global.princeyt = 'https://youtube.com/'
-global.Princelog = 'https://i.imgur.com/cUvIv5w.jpeg'
+global.Princelog = 'https://i.imgur.com/89m3j04.mp4'
 global.thumb = fs.readFileSync('./Assets/Prince.png')
 //💌------------------------------------------💌
 
